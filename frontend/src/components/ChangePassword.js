1af5bcba7e5c4cc4d080c647047e5cd3a52c962c
Gabriel <gjbarret@asu.edu>
2024-03-07 08:12:00 -0700

    first of many
    message

--- frontend/src/components/ChangePassword.js
+++ frontend/src/components/ChangePassword.js
@@ -1,1 +1,13 @@
 import { Container } from 'react-bootstrap';
+
+import React from 'react';
+
+const ChangePassword = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ChangePassword;
